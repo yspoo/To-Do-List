d31ae88d5e648e42f7e988856015c3c357662739
gloria <gloriasoh553@hotmail.com>
2017-10-17 17:38:28 +0800

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,7 @@
 Rails.application.routes.draw do
-  get 'task/index'
 
-  get 'task/show'
-
-  get 'task/new'
-
-  get 'task/create'
-
-  get 'task/update'
-
-  get 'task/edit'
-
-  get 'task/destroy'
-
-  get 'task_lists/index'
-
-  get 'task_lists/show'
-
-  get 'task_lists/new'
-
-  get 'task_lists/create'
-
-  get 'task_lists/update'
-
-  get 'task_lists/edit'
-
-  get 'task_lists/destroy'
-
+  resources :task_lists do
+  resources :tasks
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
